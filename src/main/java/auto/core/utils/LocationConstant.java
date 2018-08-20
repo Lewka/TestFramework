ddcd7c292f5466acdefafd355be167bb7af60fa9
Alexey Andreikin <isqad.bro@gmail.com>
2018-08-20 17:06:18 +0300

    Small fixes

--- src/main/java/auto/core/utils/LocationConstant.java
+++ src/main/java/auto/core/utils/LocationConstant.java
@@ -1,11 +1,7 @@
 package auto.core.utils;
 
 public enum LocationConstant {
-    PATH_TO_DATA("./data"),
-    PATH_TO_REPORT("./report"),
-    SUBFOLDER_SCREENSHOTS("./screenshots"),
-    SUBFOLDER_EXTENT_REPORT("/extent"),
-    SUBFOLDER_EMAIL_REPORT("/email");
+    SUBFOLDER_SCREENSHOTS("./screenshots");
 
     private String path;
 
